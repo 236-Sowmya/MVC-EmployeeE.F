2916ad43cc87f1b00322e79c3325525186e8ef9c
236-Sowmya <s.sowmya9494@gmail.com>
2022-01-05 12:28:52 +0530

    Add project files.

---
+++ MVC-EmployeeE.F/Data/MVC_EmployeeEFContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MVC_EmployeeE.F.Models;
+
+namespace MVC_EmployeeE.F.Data
+{
+    public class MVC_EmployeeEFContext : DbContext
+    {
+        public MVC_EmployeeEFContext (DbContextOptions<MVC_EmployeeEFContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MVC_EmployeeE.F.Models.Employee> Employee { get; set; }
+    }
+}
